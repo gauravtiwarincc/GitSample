9a2157cfab87b38eef5d4db190c50cae51e36550
Abhishek <abhishek@Abhisheks-Mac-mini.local>
2018-10-31 11:35:53 +0530

    Print Message added

---
+++ GitSample/loginManager.swift
@@ -0,0 +1,13 @@
+//
+//  loginManager.swift
+//  GitSample
+//
+//  Created by Abhishek on 31/10/18.
+//  Copyright © 2018 Abhishek. All rights reserved.
+//
+
+import UIKit
+
+class loginManager: NSObject {
+
+}
